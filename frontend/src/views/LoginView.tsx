f33ad641d8945fc9fc34f9bae903d66955df6d9d
Alexis Salgado <chimalexoficial@gmail.com>
2024-12-27 16:17:23 -0600

    form validations added

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom'
 export default function LoginView() {
     return (
         <>
-            <div>LoginView</div>
+            <h1 className='text-4xl text-white font-bold'>Login</h1>
 
-            <nav>
-                <Link to="/auth/register">
-                    Sign Up here
+            <nav className='mt-10'>
+                <Link
+                    to="/auth/register"
+                    className='text-center text-white text-lg block' >
+                    No account? Sign up here
                 </Link>
             </nav>
         </>
--- frontend/src/views/RegisterView.tsx
+++ frontend/src/views/RegisterView.tsx
@@ -1,13 +1,129 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import ErrorMessage from '../components/ErrorMessage';
+import type { RegisterForm } from '../types/index';
 
 
 export const RegisterView = () => {
+
+    const initialValues : RegisterForm = {
+        name: '',
+        email: '',
+        handle: '',
+        password: '',
+        password_confirmation: ''
+    }
+
+    const { register, watch, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues });
+
+    const password = watch('password');
+
+    console.log(errors);
+
+    const handleRegister = (formData : RegisterForm) => {
+        console.log('From hanldeRegister');
+        console.log(formData);
+    }
+
     return (
         <>
-            <div>RegisterView</div>
+            <h1 className='text-4xl text-white font-bold'>Create account</h1>
 
-            <nav>
-                <Link to="/auth/login">
+            <form
+                onSubmit={handleSubmit(handleRegister)}
+                className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10">
+                <div className="grid grid-cols-1 space-y-3">
+                    <label htmlFor="name" className="text-2xl text-slate-500">Name</label>
+                    <input
+                        id="name"
+                        type="text"
+                        placeholder="Your name"
+                        className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('name', {
+                            required: "Name is required"
+                        })}
+                    />
+                    {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage>}
+
+                </div>
+
+                <div className="grid grid-cols-1 space-y-3">
+                    <label htmlFor="email" className="text-2xl text-slate-500">E-mail</label>
+                    <input
+                        id="email"
+                        placeholder="Email"
+                        className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('email', {
+                            required: "Email is required",
+                            pattern: {
+                                value: /\S+@\S+\.\S+/,
+                                message: "Invalid email",
+                            },
+                        })}
+                    />
+                    {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
+                </div>
+
+                <div className="grid grid-cols-1 space-y-3">
+                    <label htmlFor="handle" className="text-2xl text-slate-500">Username</label>
+                    <input
+                        id="handle"
+                        type="text"
+                        placeholder="Username (handle)"
+                        className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('handle', {
+                            required: "Username is required"
+                        })}
+                    />
+
+                    {errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage>}
+                </div>
+
+                <div className="grid grid-cols-1 space-y-3">
+                    <label htmlFor="password" className="text-2xl text-slate-500">Password</label>
+                    <input
+                        id="password"
+                        type="password"
+                        placeholder="Password Account"
+                        className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('password', {
+                            required: "Password is required",
+                            minLength: {
+                                value: 8,
+                                message: "Password must be at least 8 characters"
+                            }
+                        })}
+                    />
+                    {errors.password && <ErrorMessage>{errors.password.message}</ErrorMessage>}
+                </div>
+
+                <div className="grid grid-cols-1 space-y-3">
+                    <label htmlFor="password_confirmation" className="text-2xl text-slate-500"> Repeat Password</label>
+                    <input
+                        id="password_confirmation"
+                        type="password"
+                        placeholder="Repeat Password"
+                        className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('password_confirmation', {
+                            required: "Password confirmation is required",
+                            validate: (value) => value === password || 'Password are not equal'
+                        })}
+                    />
+
+                    {errors.password_confirmation && <ErrorMessage>{errors.password_confirmation.message}</ErrorMessage>}
+                </div>
+
+                <input
+                    type="submit"
+                    className="bg-cyan-400 p-3 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
+                    value='Create my account'
+                />
+            </form>
+
+            <nav className='mt-10'>
+                <Link
+                    to="/auth/login"
+                    className='text-center text-white text-lg block'>
                     Login Now
                 </Link>
             </nav>
